62fa14603217a539d90b78350ac2f1edc3010a8b
Себастьян Перейро <m4r4d3r@hotmail.com>
2020-03-09 11:12:08 +0300

    Update Core from 2.2 to 3.1

--- Homie/Startup.cs
+++ Homie/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Pomelo.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
@@ -35,26 +36,27 @@ namespace Homie
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddTransient<IMovieRepository, EFMoviesRepository>();
+            services.AddTransient<IMovieRepository, EFMoviesRepository>();            
 
-            services.AddMvc();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {           
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute("areas","{area:exists}/{controller=Home}/{action=Index}");
+                endpoints.MapControllerRoute("default","{controller=Home}/{action=Index}");
+            });
+            
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
-            app.UseStaticFiles();
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    name: "areas",
-                    template: "{area:exists}/{controller=Home}/{action=Index}");
-
-                routes.MapRoute(
-                     name: "default",
-                     template: "{controller=Home}/{action=Index}");
-            });
+            app.UseStaticFiles();            
         
         }
     }
