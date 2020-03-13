a5af347f76b9697c3506ee7d67893f0d1da05f94
Roman <Roman@VM2WINDOWSPRO>
2020-03-13 09:59:56 +0300

    Add some for MVP

--- Homie/wwwroot/js/JavaScript.js
+++ Homie/wwwroot/js/JavaScript.js
@@ -1,11 +1,9 @@
-﻿/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-} 
+﻿<script>
+    function w3_open() {
+        document.getElementById("mySidebar").style.display = "block";
+  }
+  
+function w3_close() {
+        document.getElementById("mySidebar").style.display = "none";
+  }
+</script>
