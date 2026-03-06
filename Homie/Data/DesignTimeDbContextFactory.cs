d34139772871f4bbc739df5c2d4a089516d23eae
Martin V.C. <vargas.martin@yandex.ru>
2026-03-06 16:14:25 +0300

    Исправление (ef не видел appsettings.Development.json)

--- Homie/Data/DesignTimeDbContextFactory.cs
+++ Homie/Data/DesignTimeDbContextFactory.cs
@@ -14,9 +14,13 @@ namespace Homie.Data
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .AddEnvironmentVariables()
                 .Build();
 
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
